e55007d8025ccaf50131946100f584aa25e3ad36
jfbeatit <justforbeatit@gmail.com>
2022-04-07 14:50:01 +0800

    feat: add a-config-provider to support zhCN

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ declare module 'vue' {
     AButton: typeof import('ant-design-vue/es')['Button']
     ACard: typeof import('ant-design-vue/es')['Card']
     ACol: typeof import('ant-design-vue/es')['Col']
+    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
     ADropdown: typeof import('ant-design-vue/es')['Dropdown']
     AForm: typeof import('ant-design-vue/es')['Form']
     AFormItem: typeof import('ant-design-vue/es')['FormItem']
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import { useMenuStore } from '@/store/menu'
 import TheSider from './layout/TheSider.vue'
 import TheMenu from './layout/TheMenu.vue'
 import TheHeader from './layout/TheHeader.vue'
 import TheContent from './layout/TheContent.vue'
 import TheFooter from './layout/TheFooter.vue'
-import { useMenuStore } from '@/store/menu'
 
 const { collapsed } = storeToRefs(useMenuStore())
 </script>
@@ -15,9 +16,11 @@ const { collapsed } = storeToRefs(useMenuStore())
       <template #logo><ant-logo /></template>
       <template #menu><the-menu /></template>
     </the-sider>
-    <a-layout class="layout-main" :style="{ marginLeft: collapsed ? '48px' : '200px' }">
+    <a-layout :style="{ marginLeft: collapsed ? '48px' : '200px' }">
       <the-header />
-      <the-content />
+      <a-config-provider :locate="zhCN">
+        <the-content />
+      </a-config-provider>
       <the-footer />
     </a-layout>
   </a-layout>
