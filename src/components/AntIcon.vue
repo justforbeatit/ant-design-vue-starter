8802769b14ba4aa9c2bb63f71aeeae196d214e46
jfbeatit <justforbeatit@gmail.com>
2022-04-18 17:51:21 +0800

    fix: type support

--- src/components/AntIcon.vue
+++ src/components/AntIcon.vue
@@ -3,9 +3,8 @@ const props = defineProps<{
   is: string,
 }>()
 
-const icon = defineAsyncComponent(() => {
-  // @ts-ignore
-  return import('@ant-design/icons-vue')['then'](module => module[props.is])
+const icon = defineAsyncComponent(async () => {
+  return import('@ant-design/icons-vue').then(module => module[<AntIcon>props.is])
 })
 </script>
 
