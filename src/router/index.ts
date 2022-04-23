c4a2913b278ce079b4830b334bbcde0a45c91e8f
jfbeatit <justforbeatit@gmail.com>
2022-04-23 15:48:39 +0800

    pert: auto generate

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-//import autoload from '@/utils/router'
 import progress from '@/utils/progress'
 import routes from '~pages'
 
-console.info(routes)
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes/*: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
-      children: [
-        ...autoload()
-      ]
-    },
-    {
-      path: '/auth/login',
-      name: 'auth.login',
-      component: () => import('@/views/LoginView.vue')
-    },
-    {
-      path: '/playground',
-      name: 'test',
-      component: () => import('@/views/Playground.vue')
-    },
-  ]*/
+  routes
 })
 
 router.beforeEach((to, from) => {
