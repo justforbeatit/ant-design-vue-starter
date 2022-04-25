2d453aa8dc1147eae5c1bd3a17c27b613bead6bb
jfbeatit <justforbeatit@gmail.com>
2022-04-25 11:48:31 +0800

    perf: replace index.vue to index.ts

---
+++ src/views/index.ts
@@ -0,0 +1,43 @@
+import { Layout } from "ant-design-vue"
+import logo from '@/composables/logo'
+import TheSider from './layouts/TheSider.vue'
+import TheMenu from './layouts/TheMenu.vue'
+import TheHeader from './layouts/TheHeader.vue'
+import TheContent from './layouts/TheContent.vue'
+import TheFooter from './layouts/TheFooter.vue'
+import { useMenuStore } from '@/store/menu'
+
+export default defineComponent({
+  setup() {
+    const { collapsed } = storeToRefs(useMenuStore())
+
+    const style = computed(() => {
+      return {
+        marginLeft: collapsed.value ? '48px' : '200px',
+        transition: 'margin-left 0.1s'
+      }
+    })
+
+    return () => h(Layout, {}, () => [
+      h(
+        TheSider,
+        {},
+        {
+          logo: () => h(logo),
+          menu: () => h(TheMenu)
+        }
+      ),
+      h(
+        Layout,
+        {
+          style: style.value
+        },
+        () => [
+          h(TheHeader),
+          h(TheContent),
+          h(TheFooter)
+        ]
+      )
+    ])
+  }
+})
